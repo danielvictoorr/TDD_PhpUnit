f114a80dfc2d8be6d26f517d667290130ada10a8
daniel_victorr <danielvictor1408@gmail.com>
2024-06-04 00:02:32 -0300

    feat: first case test, an discount calculator

---
+++ discountCalculator/DiscountCalculator.php
@@ -0,0 +1,15 @@
+<?php
+
+//criando a classe que vai aplicar ou não o desconto
+class DiscountCalculator{
+    const MINIMUM_VALUE = 100;
+
+    const DISCOUNT_VALUE = 20;
+
+    public function apply($value){
+        if($value> self::MINIMUM_VALUE){
+            return $value - self::DISCOUNT_VALUE;
+        }
+        return $value;
+    }
+}
